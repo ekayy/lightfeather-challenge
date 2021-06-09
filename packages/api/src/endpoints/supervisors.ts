fa4104e2a3753bff243ca1e4a77c6b71d6349636
Eric K Wu <ekuan.wu@gmail.com>
2021-06-08 22:04:17 -0700

    Feat: GET supervisors endpoint

--- packages/api/src/endpoints/supervisors.ts
+++ packages/api/src/endpoints/supervisors.ts
@@ -1,15 +1,35 @@
 import { Request, Response } from 'express'
+import got from 'got'
+
+interface Supervisor {
+  id: string
+  phone: string
+  jurisdiction: string
+  identificationNumber: string
+  firstName: string
+  lastName: string
+}
 
 export default async function supervisors(_req: Request, res: Response) {
-  const healthcheck = {
-    uptime: process.uptime(),
-    message: 'OK',
-    timestamp: Date.now(),
-  }
+  const url =
+    'https://609aae2c0f5a13001721bb02.mockapi.io/lightfeather/managers'
+
   try {
-    res.send(healthcheck)
+    const supervisors: Supervisor[] = await got(url).json()
+    const result: string[] = []
+
+    supervisors.map(supervisor => {
+      const { jurisdiction, lastName, firstName } = supervisor
+
+      if (!/\d/.test(jurisdiction)) {
+        const formattedSupervisor = `${jurisdiction} - ${lastName}, ${firstName}`
+        result.push(formattedSupervisor)
+      }
+    })
+
+    result.sort()
+    res.send(result)
   } catch (e) {
-    healthcheck.message = e
-    res.status(503).send()
+    res.status(503).send(e.response.body)
   }
 }
